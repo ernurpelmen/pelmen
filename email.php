6485df4aa5f5b4c6e5f0e45f7b97d7d727ba969e
ernurpelmen <72154563+ernurpelmen@users.noreply.github.com>
2020-09-30 22:20:09 +0600

    Add files via upload

---
+++ email.php
@@ -0,0 +1,55 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+
+// Load Composer's autoloader
+date_default_timezone_set('Etc/UTC');
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/class.phpmailer.php';
+require 'phpmailer/class.smtp.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;
+    $mail->CharSet = 'UTF-8';                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host = gethostbyname('ssl://smtp.gmail.com');                   // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'nub0223g@gmail.com';          // SMTP username
+    $mail->Password   = 'erer0223';                               // SMTP password
+    $mail->SMTPSecure = "ssl";         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465; 
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+        );                                   // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('nub0223g@gmail.com', 'Ернур');
+    $mail->addAddress('kaltazik009@gmail.com');     // Add a recipient
+
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}, его почта: ${userEmail}";
+
+    
+    if ($mail -> send()) {
+        echo "ok";
+    }else{
+        echo "Сообщение не отправлено. Код ошибки: {$mail->ErrorInfo}";
+    }
+} catch (Exception $e) {
+    echo "Сообщение не отправлено. Код ошибки: {$mail->ErrorInfo}";
+}
